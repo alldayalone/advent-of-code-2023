d6e7babf12d54c5a04351c427a12af79ed89b7eb
Pavel Kalugin <p.kalugin@paralect.com>
2023-12-21 13:28:23 +0300

    day 5: naive solution but doesnt scale. we need better

---
+++ src/05_2/Main.hs
@@ -0,0 +1,30 @@
+import System.IO
+import Data.List.Split (splitOn, splitOneOf)
+import Data.List (find, subsequences, stripPrefix, isPrefixOf, isSuffixOf, permutations, elemIndex, intersect)
+import GHC.Plugins (fstOf3, sndOf3, count, split)
+
+main = do
+  contents <- readFile "src/05_1/input.txt"
+  (print . minimum . process . parseInput) contents
+
+process :: ([Int], [[[Int]]]) -> [Int]
+process (seeds, maps) = map (\seed -> foldl followMap seed maps) seeds
+
+followMap :: Int -> [[Int]] -> Int
+followMap x map = case find (isMatch x) map of
+  Nothing -> x
+  Just [dest, src, count] -> dest + x - src
+
+isMatch x [_, src, count]  = x >= src && x <= src + count
+
+parseInput :: String -> ([Int], [[[Int]]])
+parseInput contents = (seeds, maps)
+  where
+    (seedStr:mapsStr) = splitOn "\n\n" contents
+    seeds = (parseSeedRange . map read . splitOn " ") (splitOn ": " seedStr !! 1)
+    maps = map (map (map read . splitOn " ") . (drop 1 . splitOn "\n")) mapsStr
+
+parseSeedRange :: [Int] -> [Int]
+parseSeedRange [] = []
+parseSeedRange (seed:size:rest) = [seed..seed+size-1] ++ parseSeedRange rest
+
