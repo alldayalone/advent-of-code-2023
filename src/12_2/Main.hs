4e5b6de63b7c363706be45aa54149cb25ec3f657
Pavel Kalugin <p.kalugin@paralect.com>
2024-07-13 23:06:13 +0300

    Found mistake in unfold

--- src/12_2/Main.hs
+++ src/12_2/Main.hs
@@ -4,32 +4,35 @@ import Data.List.Split (splitOn,split)
 main :: IO ()
 main = do
   contents <- readFile "src/12_2/input_test.txt"
-  print  . map (solve' . parse) . lines $ contents
+  print .  map (solve' . parse) . lines $ contents
 
 parse :: String -> (String, [Int])
-parse line = (hotStringsStr, lengths)
+parse line = (unfoldedStrings, unfoldedNumbers)
   where 
     (hotStringsStr : lengthStr : _) = splitOn " " line
     lengths = map read $ splitOn "," lengthStr
+    unfoldedStrings = unfoldTheRecords hotStringsStr "?" 5
+    unfoldedNumbers = unfoldTheRecords lengths [] 5
 
 solve' :: (String, [Int]) -> Int
-solve' (s, i) = classicSolution * unfoldedSolution ^ 4
-  where 
-    classicSolution = solve s i Nothing
-    unfoldedSolution = solve (s++"?") i Nothing
+solve' (s, i) = solve s i Nothing (max 0 (sum i - count '#' s))
 
-solve :: String -> [Int] -> Maybe Char -> Int
-solve [] [] _ = 1
-solve [] _ _ = 0
-solve ('.':s) _ (Just '#') = 0
-solve ('.':s) i _ = solve s i Nothing
-solve ('#':s) _ (Just '.') = 0
-solve ('#':s) [] _ = 0
-solve ('#':s) (1:is) _ = solve s is (Just '.')
-solve ('#':s) (x:is) _ = solve s (x-1:is) (Just '#')
-solve ('?':s) i Nothing = solve ('?':s) i (Just '#') + solve ('?':s) i (Just '.')
-solve ('?':s) i (Just ch) = solve (ch:s) i (Just ch)
+solve :: String -> [Int] -> Maybe Char -> Int -> Int
+solve [] [] _ _ = 1
+solve [] _ _ _ = 0
+solve ('.':s) _ (Just '#') _ = 0
+solve ('.':s) i _ c = solve s i Nothing c
+solve ('#':s) _ (Just '.') _ = 0
+solve ('#':s) [] _ _ = 0
+solve ('#':s) (1:is) _ c = solve s is (Just '.') c
+solve ('#':s) (x:is) _ c = solve s (x-1:is) (Just '#') c
+solve ('?':s) i b 0 = solve ('.':s) i b 0
+solve ('?':s) i Nothing c = solve ('?':s) i (Just '#') (c-1) + solve ('?':s) i (Just '.') c
+solve ('?':s) i (Just ch) c = solve (ch:s) i (Just ch) c
 
 unfoldTheRecords :: [a] -> [a] -> Int -> [a]
 unfoldTheRecords x _ 0 = x
-unfoldTheRecords x separator a = unfoldTheRecords (x ++ separator ++ x) separator (a-1)
+unfoldTheRecords x separator a = x ++ separator ++ unfoldTheRecords x separator (a-1)
+
+count :: (Foldable t, Eq a, Num b) => a -> t a -> b
+count a = foldr (\x sum -> if x == a then sum+1 else sum) 0
