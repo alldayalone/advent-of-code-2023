4456a8e56279416ade85a5114d5f74566042b2e6
Pavel Kalugin <p.kalugin@paralect.com>
2024-07-14 10:57:22 +0300

    Memo worked!! Just had to correctly use it. Nice

--- src/12_2/Main.hs
+++ src/12_2/Main.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE DeriveGeneric, TypeOperators, TypeFamilies #-}
+{-# LANGUAGE DerivingStrategies, DeriveGeneric, TypeOperators, TypeFamilies #-}
 
 module Main (main, solve) where
 import Data.List.Split (splitOn,split)
 import Data.MemoTrie
-import GHC.Generics (Generic) 
+import GHC.Generics (Generic)
 
 main :: IO ()
 main = do
   contents <- readFile "src/12_2/input.txt"
-  mapM_ print . map (solve' . parse) . lines $ contents
+  print . sum . map (solve' . parse) . lines $ contents
 
 parse :: String -> (String, [Int])
 parse line = (unfoldedStrings, unfoldedNumbers)
@@ -21,20 +21,20 @@ parse line = (unfoldedStrings, unfoldedNumbers)
 solve' :: (String, [Int]) -> Int
 solve' (s, i) = solveMemoized (Node (s, i, Nothing))
 newtype Node = Node (String, [Int], Maybe Char)
-  deriving (Generic) 
+  deriving stock Generic
 
 solve :: Node -> Int
 solve (Node ([],      [], _       )) = 1
 solve (Node ([],       _, _       )) = 0
 solve (Node ('.':s,    _, Just '#')) = 0
-solve (Node ('.':s,   i , _       )) = solve (Node (s, i,      Nothing ))
+solve (Node ('.':s,   i , _       )) = solveMemoized (Node (s, i,      Nothing ))
 solve (Node ('#':s,   _ , Just '.')) = 0
 solve (Node ('#':s,   [], _       )) = 0
-solve (Node ('#':s, 1:is, _       )) = solve (Node (s, is,     Just '.'))
-solve (Node ('#':s, x:is, _       )) = solve (Node (s, x-1:is, Just '#')) 
-solve (Node ('?':s,    i, Just '#')) = solve (Node ('#':s, i,  Nothing ))
-solve (Node ('?':s,    i, Just '.')) = solve (Node ('.':s, i,  Nothing )) 
-solve (Node ('?':s,    i, Nothing )) = solve (Node ('.':s, i,  Nothing )) + solve (Node ('#':s,i, Nothing))
+solve (Node ('#':s, 1:is, _       )) = solveMemoized (Node (s, is,     Just '.'))
+solve (Node ('#':s, x:is, _       )) = solveMemoized (Node (s, x-1:is, Just '#')) 
+solve (Node ('?':s,    i, Just '#')) = solveMemoized (Node ('#':s, i,  Nothing ))
+solve (Node ('?':s,    i, Just '.')) = solveMemoized (Node ('.':s, i,  Nothing )) 
+solve (Node ('?':s,    i, Nothing )) = solveMemoized (Node ('.':s, i,  Nothing )) + solveMemoized (Node ('#':s,i, Nothing))
 
 instance HasTrie Node where
   newtype (Node :->: b) = NodeTrie { unNodeTrie :: Reg Node :->: b } 
