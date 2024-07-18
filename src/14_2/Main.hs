0ae96239a4090cfba375679e4d61167321eebc94
Pavel Kalugin <p.kalugin@paralect.com>
2024-07-18 17:28:28 +0300

    14_2. Write turn function

--- src/14_2/Main.hs
+++ src/14_2/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
 import Debug.Trace (trace)
 import Data.List (transpose)
@@ -6,21 +8,32 @@ import Data.List (transpose)
 -- Rewrite solve into 2 separate functions:
 --  1. Moving Os
 --  2. [x] Calculating the load
--- Write a function that takes current matrix and direction and returns the next matrix
+-- [x] Write a function that takes current matrix and direction and returns the next matrix
 -- Start iterating cycles, and find the pattern - when matrix + direction is repeated
 -- Skip unnecessary cycles mathematically
 
 main :: IO ()
 main = do
   contents <- readFile "src/14_2/input_test.txt"
-  print . map solve . parse $ contents
+  print  . parse $ contents
 
--- | Transpose to get the columns
-parse :: String -> [String]
-parse = transpose . lines
+parse :: String -> State
+parse = turn . turn . eastState . lines
 
 solve :: String -> Int
 solve = calcLoad 
+
+data Direction = North | West | South | East deriving (Show, Bounded, Eq, Enum)
+data State = State { matrix :: [String], direction :: Direction } deriving (Show, Eq)
+
+next :: (Eq a, Bounded a, Enum a) => a -> a
+next x = if x == maxBound then minBound else succ x
+
+eastState :: [String] -> State
+eastState m = State m East
+
+turn :: State -> State
+turn (State m dir) = State (transpose m) (next dir)
 
 calcLoad :: String -> Int
 -- calcLoad v | trace ("fn " ++ v) False = undefined
