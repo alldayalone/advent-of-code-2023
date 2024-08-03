f3faa8b1454e0ef2e5d52feb4f784f3e4a60ea22
Pavel Kalugin <p.kalugin@paralect.com>
2024-08-03 19:12:30 +0300

    15_2. Calculate focusing power

--- src/15_2/Main.hs
+++ src/15_2/Main.hs
@@ -1,11 +1,41 @@
 module Main (main, hash) where
 import Data.Char (ord)
 import Data.List.Split (splitOn, split, oneOf)
+import Data.Vector as Vector (Vector, (!), (//), replicate, imap, sum)
+import Debug.Trace (trace)
 
 main :: IO ()
 main = do
-  contents <- readFile "src/15_2/input_test.txt"
-  print . map parse . splitOn "," $ contents
+  contents <- readFile "src/15_2/input.txt"
+  print . focusingPower . foldl applyInstruction emptyLensSeries . map parse . splitOn "," $ contents
+
+type LensSeries = Vector [Lens]
+type Lens = (String, Int) -- (label, focal length)
+
+emptyLensSeries :: LensSeries
+emptyLensSeries = Vector.replicate 256 []
+
+applyInstruction :: LensSeries -> Instruction  -> LensSeries
+-- applyInstruction lensSeries _ | trace (show lensSeries) False = undefined
+applyInstruction lensSeries (label, Set, Just focalLength) = lensSeries // [(boxIndex, setLens box (label, focalLength))]
+  where
+    boxIndex = hash label
+    box = lensSeries ! boxIndex
+applyInstruction lensSeries (label, Remove, Nothing) = lensSeries // [(boxIndex, removeLens box label)]
+  where
+    boxIndex = hash label
+    box = lensSeries ! boxIndex
+
+setLens :: [Lens] -> Lens -> [Lens]
+setLens [] newLens = [newLens]
+setLens (lens:box) newLens = if fst lens == fst newLens then newLens:box else lens:setLens box newLens
+
+removeLens :: [Lens] -> String -> [Lens]
+removeLens [] _ = []
+removeLens (lens:box) label = if fst lens == label then box else lens:removeLens box label
+
+focusingPower :: LensSeries -> Int
+focusingPower = Vector.sum . Vector.imap (\i box -> (i + 1) * (Prelude.sum . zipWith (\slot lens -> slot * snd lens) [1..] $ box)) 
 
 type Instruction = (String, Op, Maybe Int) -- (label, operation, focal length)
 data Op = Set | Remove deriving (Show)
@@ -24,3 +54,4 @@ hash = hash' 0
     hash' :: Int -> String -> Int
     hash' currentValue [] = currentValue
     hash' val (x:xs) = hash' (flip mod 256 . (* 17) . (+ val) . ord $ x) xs
+
