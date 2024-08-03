e2a4f6afda8f73cdfc6cddb34df3c153e2a15505
Pavel Kalugin <p.kalugin@paralect.com>
2024-08-03 23:54:09 +0300

    16_2. Setup

---
+++ src/16_2/Cell.hs
@@ -0,0 +1,39 @@
+module Cell (Cell(..), empty, north, west, east, south, addLight, lightExists, energized) where
+
+import Data.Bits
+
+data Cell = Cell { 
+  char  :: Char, -- Possible values: empty space (.), mirrors (/ and \), and splitters (| and -).
+  light :: Int -- 0000 each bit contains light direction : north, east, south, west. E.g. 5 = 0101 means east and west 
+}
+
+
+empty :: Char -> Cell
+empty char = Cell { char = char, light = 0 }
+
+addLight :: Int -> Cell -> Cell
+addLight direction cell = cell { light = light cell .|. direction }
+north :: Int
+north = 1
+east :: Int
+east = 2
+south :: Int
+south = 4
+west :: Int
+west = 8
+
+lightExists :: Int -> Cell  -> Bool
+lightExists direction cell = light cell .&. direction /= 0
+
+energized :: Cell -> Int 
+energized cell = if light cell /= 0 then 1 else 0
+
+instance Show Cell where
+  -- show (Cell _ light) = if light == 0 then "." else "#" -- view energized cells
+  show (Cell '.' 0) = "."
+  show (Cell '.' 1) = "^"
+  show (Cell '.' 2) = ">"
+  show (Cell '.' 4) = "v"
+  show (Cell '.' 8) = "<"
+  show (Cell '.' light) = show . popCount $ light
+  show (Cell char _) = [char]
---
+++ src/16_2/Main.hs
@@ -0,0 +1,52 @@
+module Main (main) where
+import Cell (Cell(..), addLight, north, empty, lightExists, energized)
+import Data.Matrix as Matrix (Matrix, fromLists, setElem, (!), ncols, nrows)
+import Debug.Trace (trace)
+
+-- Algo draft
+-- 1. pop the queue
+-- 2. get new candidates based on the cell
+-- 3. update states
+-- 4. continue while queue is not empty
+
+main :: IO ()
+main = do
+  contents <- readFile "src/16_2/input_test.txt"
+  print . sum . fmap Cell.energized . tick [((1,1),2)] . parse $ contents
+
+parse = fmap Cell.empty . Matrix.fromLists . lines
+
+tick :: [((Int, Int), Int)] -> Matrix Cell -> Matrix Cell
+-- tick ((pos, dir):xs) _ | trace (show (pos, dir)) False = undefined
+tick [] matrix = matrix
+tick ((pos, dir):xs) matrix = if skip (pos, dir) matrix then tick xs matrix else tick (newCandidates ++ xs) $ Matrix.setElem cell pos matrix
+  where
+    cell = addLight dir (matrix Matrix.! pos)
+    newCandidates = case (char cell, dir) of
+      ('.', 1) -> [((fst pos - 1, snd pos), 1)]
+      ('.', 2) -> [((fst pos, snd pos + 1), 2)]
+      ('.', 4) -> [((fst pos + 1, snd pos), 4)]
+      ('.', 8) -> [((fst pos, snd pos - 1), 8)]
+      ('/', 1) -> [((fst pos, snd pos + 1), 2)]
+      ('/', 2) -> [((fst pos - 1, snd pos), 1)]
+      ('/', 4) -> [((fst pos, snd pos - 1), 8)]
+      ('/', 8) -> [((fst pos + 1, snd pos), 4)]
+      ('\\', 1) -> [((fst pos, snd pos - 1), 8)]
+      ('\\', 2) -> [((fst pos + 1, snd pos), 4)]
+      ('\\', 4) -> [((fst pos, snd pos + 1), 2)]
+      ('\\', 8) -> [((fst pos - 1, snd pos), 1)]
+      ('|', 2) -> [((fst pos - 1, snd pos), 1), ((fst pos + 1, snd pos), 4)]
+      ('|', 8) -> [((fst pos - 1, snd pos), 1), ((fst pos + 1, snd pos), 4)]
+      ('|', 1) -> [((fst pos - 1, snd pos), 1)]
+      ('|', 4) -> [((fst pos + 1, snd pos), 4)]
+      ('-', 1) -> [((fst pos, snd pos - 1), 8), ((fst pos, snd pos + 1), 2)]
+      ('-', 4) -> [((fst pos, snd pos - 1), 8), ((fst pos, snd pos + 1), 2)]
+      ('-', 2) -> [((fst pos, snd pos + 1), 2)]
+      ('-', 8) -> [((fst pos, snd pos - 1), 8)]
+      _ -> []
+
+invalidBounds :: (Int, Int) -> Matrix Cell -> Bool
+invalidBounds (x, y) matrix = x < 1 || y < 1 || x > Matrix.ncols matrix || y > Matrix.nrows matrix
+
+skip :: ((Int, Int), Int) -> Matrix Cell -> Bool
+skip (pos, dir) matrix = invalidBounds pos matrix || Cell.lightExists dir (matrix Matrix.! pos)
