d34d9a5c0c3d60e855f9d3f76cb7b0a67e7d3b80
Pavel Kalugin <p.kalugin@paralect.com>
2024-08-10 15:09:53 +0300

    17_1. Working solution but slow. Need more rejections

--- src/17_1/Main.hs
+++ src/17_1/Main.hs
@@ -1,66 +1,89 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
 
 module Main (main) where
 import Debug.Trace (trace)
 
 import Data.Matrix as Matrix (Matrix(..), (!), setElem, fromLists)
 import Data.Char (digitToInt)
+import Data.Maybe as Maybe (isJust)
+import Data.Sequence as Sequence (Seq (..), findIndexL, singleton, ViewR, empty, viewr)
+
 main :: IO ()
 main = do
-  contents <- readFile "src/17_1/input_test2.txt"
+  contents <- readFile "src/17_1/input_test.txt"
   print . solve . parse $ contents
 
 parse = initialState . fmap initialCell . Matrix.fromLists . lines
 
-data Cell = Cell { 
-  h :: Int, -- heat loss
-  v :: Bool -- visited
-} deriving (Show)
+data Cell = Cell {
+  heatLoss :: Int,
+  minResult :: Int
+}
+
+instance Show Cell where
+  show Cell { heatLoss, minResult } = show minResult
+
 initialCell :: Char -> Cell
-initialCell char = Cell { h = digitToInt char, v = False }
+initialCell char = Cell { heatLoss = digitToInt char, minResult = maxBound }
 
 data Direction = North | East | South | West deriving (Show)
-data State = State { 
-  g :: Matrix Cell, -- grid
-  c :: (Direction, (Int, Int)), -- current (dir, pos) 
-  th :: Int, -- total heat lost
-  s :: Int -- steps count
-} deriving (Show)
-initialState :: Matrix Cell -> State
-initialState g = State { g = g, c = (West, (1, 1)), th = 0, s = 0 }
+data State = State {
+  grid :: Matrix Cell,
+  path :: Seq (Direction, (Int, Int))
+}
 
-solve :: State -> Int
--- turn s | trace ("turn " ++ show s) False = undefined
-solve s | trace ("solve " ++ show s) False = undefined
-solve State { g, c = (d, p), th, s }
-  | invalidBounds p g || v cell = maxBound
-  | fst p == Matrix.ncols g && snd p == Matrix.nrows g = th
-  | otherwise = minimum $ map solve candidateStates
+instance Show State where
+  show State { grid, path } = show grid
+
+initialState :: Matrix Cell -> State
+initialState grid = State { grid = zeroFirstCell grid, path = Sequence.singleton (East, (1, 1)) }
+
+zeroFirstCell :: Matrix Cell -> Matrix Cell
+zeroFirstCell = Matrix.setElem (Cell { heatLoss = 0, minResult = 0 }) (1, 1)
+
+solve :: State -> State
+solve s | trace (show s) False = undefined
+solve State { grid, path }
+  | invalidBounds pos grid = State { grid, path = initPath }
+  | newHeatLoss' > minResult cell = State { grid, path = initPath }
+  | Maybe.isJust (Sequence.findIndexL (\(_, p) -> p == pos) initPath) = State { grid, path = initPath }
+  | pos == (Matrix.nrows grid, Matrix.ncols grid) = State { grid = grid', path }
+  | otherwise = foldl mainFolder (State { grid = grid', path }) candidatePaths
   where
-      cell = g Matrix.! p
-      th' = th + h cell
-      s' = s + 1
-      g' = Matrix.setElem (Cell {h = h cell, v = True}) p g
-      candidateDirections = map (\f -> f (d, p)) [goStraight, turnLeft, turnRight]
-      candidateStates = map (\c' -> State {g = g', c = c', th = th', s = s'}) candidateDirections
-            
+    (initPath:|>(dir, pos)) = path
+    cell = grid Matrix.! pos
+    newHeatLoss' = newHeatLoss path grid
+    cell' = Cell { heatLoss = heatLoss cell, minResult = newHeatLoss' }
+    grid' = Matrix.setElem cell' pos grid
+    candidateDirections = map (\f -> f (dir, pos)) [goStraight, turnRight, turnLeft]
+    candidatePaths = map (path :|>) candidateDirections
+
+mainFolder :: State -> Seq (Direction, (Int, Int)) -> State
+mainFolder (State { grid , path }) path' = solve (State { grid = grid, path = path' })
+
+newHeatLoss :: Seq (Direction, (Int, Int)) -> Matrix Cell -> Int
+newHeatLoss Sequence.Empty grid = 0
+newHeatLoss (_:|>(_, prevPos):|>(dir, pos)) grid = minResult (grid Matrix.! prevPos) + heatLoss (grid Matrix.! pos)
+newHeatLoss (_:|>(dir, pos)) grid = heatLoss $ grid Matrix.! pos
+
 invalidBounds :: (Int, Int) -> Matrix Cell -> Bool
 invalidBounds (x, y) matrix = x < 1 || y < 1 || x > Matrix.ncols matrix || y > Matrix.nrows matrix
 
 goStraight :: (Direction, (Int, Int)) -> (Direction, (Int, Int))
-goStraight (North, (x, y)) = (North, (x, y - 1))
-goStraight (East, (x, y)) = (East, (x + 1, y))
-goStraight (South, (x, y)) = (South, (x, y + 1))
-goStraight (West, (x, y)) = (West, (x - 1, y))
+goStraight (North, (y, x)) = (North, (y - 1, x))
+goStraight (East, (y, x)) = (East, (y, x + 1))
+goStraight (South, (y, x)) = (South, (y + 1, x))
+goStraight (West, (y, x)) = (West, (y, x - 1))
 
 turnLeft :: (Direction, (Int, Int)) -> (Direction, (Int, Int))
-turnLeft (North, (x, y)) = (West, (x - 1, y))
-turnLeft (East, (x, y)) = (North, (x, y - 1))
-turnLeft (South, (x, y)) = (East, (x + 1, y))
-turnLeft (West, (x, y)) = (South, (x, y + 1))
+turnLeft (North, (y, x)) = (West, (y, x - 1))
+turnLeft (East, (y, x)) = (North, (y - 1, x))
+turnLeft (South, (y, x)) = (East, (y, x + 1))
+turnLeft (West, (y, x)) = (South, (y + 1, x))
 
 turnRight :: (Direction, (Int, Int)) -> (Direction, (Int, Int))
-turnRight (North, (x, y)) = (East, (x + 1, y))
-turnRight (East, (x, y)) = (South, (x, y + 1))
-turnRight (South, (x, y)) = (West, (x - 1, y))
-turnRight (West, (x, y)) = (North, (x, y - 1))
+turnRight (North, (y, x)) = (East, (y, x + 1))
+turnRight (East, (y, x)) = (South, (y + 1, x))
+turnRight (South, (y, x)) = (West, (y, x - 1))
+turnRight (West, (y, x)) = (North, (y - 1, x))
