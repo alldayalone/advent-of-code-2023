a9052d10e983521acfd817a92c67155a469c0ef2
Pavel Kalugin <p.kalugin@paralect.com>
2024-09-08 11:09:18 +0300

    18_1. Parse instructions

--- src/18_1/Main.hs
+++ src/18_1/Main.hs
@@ -5,4 +5,14 @@ main = do
   contents <- readFile "src/18_1/input_test.txt"
   print . parse $ contents
 
-parse = lines
+type Color = String
+type Direction = String
+type Instruction = (Direction, Int, Color)
+
+parse :: String -> [Instruction]
+parse = map parseLine . lines
+
+parseLine :: String -> Instruction
+parseLine line = (direction, read number, color)
+  where
+    [direction, number, color] = words line
