39e0c59eff25e98100c29b6238ea02b50820f8fb
Pavel Kalugin <p.kalugin@paralect.com>
2024-09-08 14:31:40 +0300

    18_1. Perimeter - refactoring done

--- src/18_1/Main.hs
+++ src/18_1/Main.hs
@@ -25,9 +25,9 @@ buildMatrix = fst . foldl applyInstruction (Matrix.fromLists [["#"]], (1, 1))
 
 applyInstruction :: (Matrix String, (Int, Int)) -> Instruction -> (Matrix String, (Int, Int))
 applyInstruction (m, (x, y)) (direction, number, color) = (m'', pos')
-  where 
+  where
     m' = uncurry (Matrix.extendTo ".") pos' m
-    m'' = foldr (Matrix.setElem "#") m' (posrange (x,y) pos') 
+    m'' = foldr (Matrix.setElem "#") m' (posrange (x,y) pos')
     pos' = case direction of
       "R" -> (x, y + number)
       "L" -> (x, y - number)
@@ -37,11 +37,10 @@ applyInstruction (m, (x, y)) (direction, number, color) = (m'', pos')
 
 solve :: Matrix String -> (Matrix String, [(Int, Int)])
 solve m = (m, p)
-  where 
+  where
     p = filter (\pos -> m Matrix.! pos == ".") $ perimeter (1, 1) (Matrix.nrows m, Matrix.ncols m)
 
 -- Utils
-
 posrange :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
 posrange (x1, y1) (x2, y2)
   | x1 > x2 = map (,y1) [x1-1,x1-2..x2]
@@ -50,4 +49,10 @@ posrange (x1, y1) (x2, y2)
   | y1 > y2 = map (x1,) [y1-1,y1-2..y2]
 
 perimeter :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
-perimeter (x1, y1) (x2, y2) = posrange (x1, y1) (x1, y2) ++ posrange (x1, y2) (x2, y2) ++ posrange (x2, y2) (x2, y1) ++ posrange (x2, y1) (x1, y1)
+perimeter (x1, y1) (x2, y2) = concat $ zipWith posrange pts (rotate 1 pts)
+  where 
+    pts = [(x1, y1), (x1, y2), (x2, y2), (x2, y1)]
+
+-- Amazing *v* https://stackoverflow.com/a/55743500
+rotate :: Int -> [a] -> [a]
+rotate  =  drop <> take
