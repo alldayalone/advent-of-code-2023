d96f43cf4be0c38cbc6aaefda6e984abb5df9057
Pavel Kalugin <p.kalugin@paralect.com>
2026-07-07 14:10:39 +0300

    [20.1] Pretty show list

--- src/20_1/Main.hs
+++ src/20_1/Main.hs
@@ -1,18 +1,37 @@
 module Main (main) where
 import Data.Time
-import Text.Show.Pretty (ppShow)
+import Text.Show.Pretty (ppShowList)
 
 main :: IO ()
 main = do
   utcNow   <- getCurrentTime
   contents <- readFile "src/20_1/input_test.txt"
-  writeFile ("src/20_1/output" ++ show utcNow ++ ".txt") . ppShow  . solve . parse $ contents
+  writeFile ("src/20_1/output" ++ show utcNow ++ ".txt") . ppShowList . solve . parse $ contents
 
 type Input = String
-type Output = String
+type Output = Log
 
 parse :: String -> Input
 parse = id
 
 solve :: Input -> Output
-solve = id
+solve _ = replicate 10 LogEntry { from = "broadcaster", signal = LOW, to = "a" }
+
+
+data Signal = HIGH | LOW 
+  deriving Show
+
+data LogEntry = LogEntry 
+  { from :: String,
+    to :: String,
+    signal :: Signal }
+instance Show LogEntry where
+  show (LogEntry { from = from, to = to, signal = signal}) = from ++ " -" ++ show signal ++ "-> " ++ to
+
+
+type Log = [LogEntry]
+type State = String
+type Queue = String
+
+tick :: Log -> State -> Queue -> Log
+tick log state queue = log
