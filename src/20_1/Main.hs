52d9a62765839d88d182f16caedc822e15684003
Pavel Kalugin <p.kalugin@paralect.com>
2026-07-07 17:10:53 +0300

    [20.1] WIP

--- src/20_1/Main.hs
+++ src/20_1/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE InstanceSigs #-}
 module Main (main) where
 import Data.Time
 import Text.Show.Pretty (ppShowList)
+import Data.List (intercalate)
+import Data.HashMap.Strict as HashMap (HashMap, fromList, lookup)
+import Data.Maybe (fromJust)
+import Data.Char
 
 main :: IO ()
 main = do
@@ -15,23 +20,59 @@ parse :: String -> Input
 parse = id
 
 solve :: Input -> Output
-solve _ = replicate 10 LogEntry { from = "broadcaster", signal = LOW, to = "a" }
+solve _ = tick [] initState [initSignal]
 
+initSignal :: Signal
+initSignal = Signal { from = "button", signalKind = Low, to = "broadcaster" }
+initState :: State
+initState = HashMap.fromList 
+  [ ("broadcaster", Module { name = "broadcaster", moduleKind = Broadcast, destinations = ["a", "b", "c"], activated = False })
+  , ("a", Module { name = "a", moduleKind = FlipFlop, destinations = ["b"], activated = False}) 
+  , ("b", Module { name = "b", moduleKind = FlipFlop, destinations = ["c"], activated = False}) 
+  , ("c", Module { name = "c", moduleKind = FlipFlop, destinations = ["inv"], activated = False})
+  , ("inv", Module { name = "inv", moduleKind = Conjunction, destinations = ["a"], activated = False}) ]
 
-data Signal = HIGH | LOW 
-  deriving Show
+data SignalKind = High | Low 
+  deriving (Show, Eq)
 
-data LogEntry = LogEntry 
+data Signal = Signal 
   { from :: String,
     to :: String,
-    signal :: Signal }
-instance Show LogEntry where
-  show (LogEntry { from = from, to = to, signal = signal}) = from ++ " -" ++ show signal ++ "-> " ++ to
+    signalKind :: SignalKind }
+instance Show Signal where
+  show (Signal { from = from, to = to, signalKind = signalKind}) = from ++ " -" ++ (map toLower . show) signalKind ++ "-> " ++ to
 
+data ModuleKind = Broadcast | FlipFlop | Conjunction
+  deriving (Eq)
+instance Show ModuleKind where
+  show :: ModuleKind -> String
+  show Broadcast = ""
+  show FlipFlop = "%"
+  show Conjunction = "&"
+data Module = Module
+  { moduleKind :: ModuleKind,
+    name :: String,
+    destinations :: [String],
+    activated :: Bool }
+instance Show Module where
+  show (Module { moduleKind = moduleKind, name = name, destinations = destinations }) = show moduleKind ++ name ++ " -> " ++ intercalate ", " destinations
 
-type Log = [LogEntry]
-type State = String
-type Queue = String
+type Log = [Signal]
+type State = HashMap String Module
+type Queue = [Signal]
 
 tick :: Log -> State -> Queue -> Log
-tick log state queue = log
+tick log state [] = log
+tick log state (signal@(Signal { from=from, to=to, signalKind=signalKind }):queue) = tick (log ++ [signal]) state (queue ++ newSignals)
+  where 
+    mod@(Module { name=name, destinations=destinations, moduleKind=moduleKind, activated=activated }) = fromJust $ HashMap.lookup to state
+    newSignals
+      | moduleKind == Broadcast = map (\dest -> Signal { from = to, to = dest, signalKind = signalKind }) destinations
+      | moduleKind == FlipFlop && signalKind == High = []
+      | moduleKind == FlipFlop && signalKind == Low = map (\dest -> Signal { from = to, to = dest, signalKind = flipFlopSignal activated }) destinations
+
+    newState = state
+
+flipFlopSignal :: Bool -> SignalKind
+flipFlopSignal False = High
+toggle True = Low
