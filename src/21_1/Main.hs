b51b4a471e7fe4c8e64ea194de7a2a9b74eefdce
Pavel Kalugin <p.kalugin@paralect.com>
2026-07-26 23:14:46 +0300

    [21.1] Step

--- src/21_1/Main.hs
+++ src/21_1/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Main (main) where
-import Data.Matrix as Matrix (Matrix, fromLists)
+import Data.Matrix as Matrix (Matrix, fromLists, mapPos)
 import Control.Arrow ((&&&), (>>>), second)
 import Data.Time
+import Data.Maybe 
 
 main :: IO ()
 main = do
@@ -20,11 +22,23 @@ parse = lines >>> (Matrix.fromLists >>> fmap parseChar) &&& findAllPos (=='S')
     parseChar _ = Just 0
 
 findAllPos :: forall a. (a -> Bool) -> [[a]] -> [Pos]
-findAllPos cond lists = foldr f [] (zip lists [0..])
+findAllPos cond lists = foldr f [] (zip lists [1..])
   where 
     f :: ([a], Int) -> [Pos] -> [Pos]
     f (ls, x) acc = acc ++ fmap (id &&& const x) matches
       where
-        matches = fmap snd . filter (\(element, _) -> cond element) $ zip ls [0..]
+        matches = fmap snd . filter (\(element, _) -> cond element) $ zip ls [1..]
 
-solve = id
+solve :: (Matrix (Maybe Int), [Pos]) -> Matrix (Maybe Int)
+solve (m, queue) = foldr solve' m queue
+
+solve' ::  Pos -> Matrix (Maybe Int) -> Matrix (Maybe Int)
+solve' (x1, y1) = Matrix.mapPos mapper
+  where 
+    mapper (x2, y2) element
+      | isNothing element = element
+      | x2 == x1-1 && y2 == y1 = fmap (+1) element
+      | x2 == x1+1 && y2 == y1 = fmap (+1) element
+      | x2 == x1 && y2 == y1-1 = fmap (+1) element
+      | x2 == x1 && y2 == y1+1 = fmap (+1) element
+      | otherwise = element
