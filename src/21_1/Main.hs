01de1ef35c997d5d92a04d0af4e3218f4cf555ec
Pavel Kalugin <p.kalugin@paralect.com>
2026-07-26 23:40:33 +0300

    [21.1] Propagate with Dijkstra style

--- src/21_1/Main.hs
+++ src/21_1/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ViewPatterns #-}
 
 module Main (main) where
 import Data.Matrix as Matrix (Matrix, fromLists, mapPos)
@@ -13,32 +12,48 @@ main = do
   contents <- readFile "src/21_1/input_test.txt"
   writeFile ("src/21_1/output" ++ show utcNow ++ ".txt") . show . solve . parse $ contents
 
-type Pos = (Int, Int)
 
-parse :: String -> (Matrix (Maybe Int), [Pos])
-parse = lines >>> (Matrix.fromLists >>> fmap parseChar) &&& findAllPos (=='S')
+type Pos = (Int, Int)
+type TimeToReach = Int
+type Elem = (Pos, TimeToReach) 
+
+maxTime :: Int
+maxTime = 99
+-- maxTime = maxBound
+
+parse :: String -> (Matrix (Maybe Int), [Elem])
+parse = lines >>> fmap (fmap parseChar) >>> Matrix.fromLists &&& findAllPos isEntryPoint
   where 
     parseChar '#' = Nothing
-    parseChar _ = Just 0
+    parseChar 'S' = Just 0
+    parseChar _ = Just maxTime
 
-findAllPos :: forall a. (a -> Bool) -> [[a]] -> [Pos]
+    isEntryPoint :: Maybe Int -> Bool
+    isEntryPoint x
+      | fmap (< maxTime) x == Just True = True
+      | otherwise = False
+
+findAllPos :: (Maybe Int -> Bool) -> [[Maybe Int]] -> [Elem]
 findAllPos cond lists = foldr f [] (zip lists [1..])
   where 
-    f :: ([a], Int) -> [Pos] -> [Pos]
-    f (ls, x) acc = acc ++ fmap (id &&& const x) matches
+    f :: ([Maybe Int], Int) -> [Elem] -> [Elem]
+    f (ls, y) acc = acc ++ matches
       where
-        matches = fmap snd . filter (\(element, _) -> cond element) $ zip ls [1..]
+        matches = fmap (\(element, x) -> ((x, y), fromJust element)) . filter (\(element, _) -> cond element) $ zip ls [1..]
 
-solve :: (Matrix (Maybe Int), [Pos]) -> Matrix (Maybe Int)
-solve (m, queue) = foldr solve' m queue
+solve :: (Matrix (Maybe Int), [Elem]) -> Matrix (Maybe Int)
+solve (m, queue) = foldr propagate m queue
 
-solve' ::  Pos -> Matrix (Maybe Int) -> Matrix (Maybe Int)
-solve' (x1, y1) = Matrix.mapPos mapper
+
+-- propagateAll (Matrix (Maybe Int), [Pos]) -> Matrix (Maybe Int)
+
+propagate :: Elem -> Matrix (Maybe Int) -> Matrix (Maybe Int)
+propagate ((x1, y1), ttr) = Matrix.mapPos mapper
   where 
     mapper (x2, y2) element
       | isNothing element = element
-      | x2 == x1-1 && y2 == y1 = fmap (+1) element
-      | x2 == x1+1 && y2 == y1 = fmap (+1) element
-      | x2 == x1 && y2 == y1-1 = fmap (+1) element
-      | x2 == x1 && y2 == y1+1 = fmap (+1) element
-      | otherwise = element
+      | x2 == x1-1 && y2 == y1 = fmap (min (ttr + 1)) element
+      | x2 == x1+1 && y2 == y1 = fmap (min (ttr + 1)) element
+      | x2 == x1 && y2 == y1-1 = fmap (min (ttr + 1)) element
+      | x2 == x1 && y2 == y1+1 = fmap (min (ttr + 1)) element
+      | otherwise = Just maxTime
