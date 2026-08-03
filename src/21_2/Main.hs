6acf2fd021c08023723f1e2bc327282ceeaba940
Pavel Kalugin <p.kalugin@paralect.com>
2026-08-03 13:44:16 +0300

    [21.2] So, did some "rhimbus radius" calculation - appears too low. no idea

--- src/21_2/Main.hs
+++ src/21_2/Main.hs
@@ -10,7 +10,7 @@ main :: IO ()
 main = do
   utcNow   <- getCurrentTime
   contents <- readFile "src/21_2/input.txt"
-  writeFile ("src/21_2/output" ++ show utcNow ++ ".txt") . show . (id &&& countResults) . solve . parse $ contents
+  writeFile ("src/21_2/output" ++ show utcNow ++ ".txt") . show . metasolve . parse $ contents
 
 
 type Pos = (Int, Int)
@@ -29,24 +29,72 @@ instance Show Cell where
 type Field = Matrix Cell
 
 
+-- metasolve :: Matrix Cell -> [Int]
+-- metasolve field = [stepsPartialSide, stepsPartialMixedSide, n, n_half, n_half_index, m, r, nFullEven, countFullEven, nFullOdd, countFullOdd, nPartialSide, countS, countN, countE, countW, nPartialMixedSide, countSE,  countSW, countNE, countNW] -- nFullEven * countFullEven + nFullOdd * countFullOdd + nPartialSide * (countS + countN + countE + countW) + nPartialMixedSide * (countSE + countSW + countNE + countNW)
+metasolve field = nFullEven * countFullEven + nFullOdd * countFullOdd + nPartialSide * (countS + countN + countE + countW) + nPartialMixedSide * (countSE + countSW + countNE + countNW)
+  where
+    n = Matrix.ncols field -- размер матрицы
+    n_half = n `div` 2
+    n_half_index = n_half + 1
+    m = nTotalSteps -- общее кол-во шагов
+    r = (m + 1) `div` n -- "радиус" заполнения ромба
+    nFullEven = (r - r `mod` 2) ^ 2 -- кол-во четных заполненных полей (n x n)
+    nFullOdd = (r - (r+1) `mod` 2) ^ 2 -- кол-во нечетных заполненных (например, центр)
+    nPartialSide = 1 -- старт с (0, (n-1)/2) и тд
+    nPartialMixedSide = max (r-1) 0 -- старт с (0, 0) и тд
+
+    solvedFieldCenter = solve n (Matrix.setElem (Only 0) (n_half_index, n_half_index) field)
+    countFullOdd = countResults doesCountEven solvedFieldCenter
+    countFullEven = countResults doesCountOdd solvedFieldCenter
+
+    stepsPartialSide = m - n_half - (r-1)*n - 1
+    stepsPartialMixedSide = m - 2*n_half - (r-2)*n - 2
+    solvedFieldS = solve stepsPartialSide (Matrix.setElem (Only 0) (1, n_half_index) field)
+    solvedFieldN = solve stepsPartialSide (Matrix.setElem (Only 0) (n, n_half_index) field)
+    solvedFieldE = solve stepsPartialSide (Matrix.setElem (Only 0) (n_half_index, 1) field)
+    solvedFieldW = solve stepsPartialSide (Matrix.setElem (Only 0) (n_half_index, n) field)
+
+    solvedFieldSE = solve stepsPartialMixedSide (Matrix.setElem (Only 0) (1, 1) field)
+    solvedFieldSW = solve stepsPartialMixedSide (Matrix.setElem (Only 0) (1, n) field)
+    solvedFieldNE = solve stepsPartialMixedSide (Matrix.setElem (Only 0) (n, 1) field)
+    solvedFieldNW = solve stepsPartialMixedSide (Matrix.setElem (Only 0) (n, n) field)
+
+    doesCountSide = if odd r then doesCountEven else doesCountOdd
+    countS = countResults doesCountSide solvedFieldS 
+    countN = countResults doesCountSide solvedFieldN 
+    countE = countResults doesCountSide solvedFieldE 
+    countW = countResults doesCountSide solvedFieldW 
+
+    doesCountMixedSide = if even r then doesCountEven else doesCountOdd
+    countSE = countResults doesCountMixedSide solvedFieldSE 
+    countSW = countResults doesCountMixedSide solvedFieldSW 
+    countNE = countResults doesCountMixedSide solvedFieldNE 
+    countNW = countResults doesCountMixedSide solvedFieldNW 
+
+
 isWall Wall = True
 isWall _ = False
 isVisitable = not . isWall
 
-doesCount :: Cell -> Bool
-doesCount Wall = False
-doesCount Unvisited = False
-doesCount (Only x) = even (x + nSteps)
-doesCount (Both x y) = True
+doesCountEven :: Cell -> Bool
+doesCountEven Wall = False
+doesCountEven Unvisited = False
+doesCountEven (Only x) = even (x + nTotalSteps)
+doesCountEven (Both x y) = True
 
-nSteps :: Int
-nSteps = 130
+doesCountOdd :: Cell -> Bool
+doesCountOdd Wall = False
+doesCountOdd Unvisited = False
+doesCountOdd (Only x) = odd (x + nTotalSteps)
+doesCountOdd (Both x y) = True
+
+nTotalSteps :: Int
+nTotalSteps = 26501365
 
 parse :: String -> Field
 parse = lines >>> fmap (fmap parseChar) >>> Matrix.fromLists
   where 
     parseChar '#' = Wall
-    parseChar 'S' = Only 0
     parseChar _ = Unvisited
 
 findAllPos :: Int -> Field -> [Elem]
@@ -61,11 +109,11 @@ findAllPos stepCount = fmap fromJust . filter isJust . Matrix.toList . Matrix.ma
     isEntryPoint :: Int -> Bool
     isEntryPoint = (== stepCount)
 
-countResults :: Field -> Int
-countResults = length . filter id . fmap doesCount . Matrix.toList
+countResults :: (Cell -> Bool) -> Field -> Int
+countResults doesCount = length . filter id . fmap doesCount . Matrix.toList
 
-solve :: Field -> Field
-solve m = fst . (!! nSteps) . iterate step $ (m, 0)
+solve :: Int -> Field -> Field
+solve nSteps m = fst . (!! nSteps) . iterate step $ (m, 0)
 
 step :: (Field, Int) -> (Field, Int)
 step (m, stepCount) = (propagateAll m (findAllPos stepCount m), stepCount + 1)
