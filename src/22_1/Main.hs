a08e6574d66526afdf7f7bb36ec5940a8f6de2cf
Pavel Kalugin <p.kalugin@paralect.com>
2026-08-07 15:03:42 +0300

    [22.1] Find supporters

--- src/22_1/Main.hs
+++ src/22_1/Main.hs
@@ -6,12 +6,13 @@ import Control.Arrow
 import Data.Time
 import Data.List.Split
 import Text.Show.Pretty
+import Data.Sort
 
 main :: IO ()
 main = do
   utcNow   <- getCurrentTime
   contents <- readFile "src/22_1/input_test.txt"
-  writeFile ("src/22_1/output" ++ show utcNow ++ ".txt") . ppShowList . solve . parse $ contents
+  writeFile ("src/22_1/output" ++ show utcNow ++ ".txt") . ppShowList . solve . settle . parse $ contents
 
 data Vec3 = Vec3
   { x :: Int,
@@ -32,4 +33,14 @@ parse = lines >>> fmap (splitOneOf ",~" >>> fmap read >>> toBlock)
   where toBlock [x1,y1,z1,x2,y2,z2] = Block { start = Vec3 { x=x1, y=y1, z=z1 }, end = Vec3 { x=x2, y=y2, z=z2 }}
 
 
-solve = id
+settle = sortOn (start >>> z) 
+
+findSupporters :: Block -> [Block] -> [Block]
+findSupporters b = filter (isSupporter b)
+  
+-- First arg - the upper block, second arg - porential supporter block
+isSupporter :: Block -> Block -> Bool
+isSupporter (Block {..}) (Block { start=start2, end=end2}) = x end >= x start2 && x start <= x end2 && y end >= y start2 && y start <= y end2 && z start == z end2 + 1
+
+solve :: [Block] -> [[Block]]
+solve bs = [findSupporters b bs | b <- bs]
