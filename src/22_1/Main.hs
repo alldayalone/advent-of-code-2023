978059278f8c7704aae004575224b231377306a3
Pavel Kalugin <p.kalugin@paralect.com>
2026-08-07 14:27:37 +0300

    [22.1] Parse blocks

--- src/22_1/Main.hs
+++ src/22_1/Main.hs
@@ -1,14 +1,35 @@
+-- {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 import Control.Arrow
 import Data.Time
 import Data.List.Split
+import Text.Show.Pretty
 
 main :: IO ()
 main = do
   utcNow   <- getCurrentTime
   contents <- readFile "src/22_1/input_test.txt"
-  writeFile ("src/22_1/output" ++ show utcNow ++ ".txt") . show . solve . parse $ contents
+  writeFile ("src/22_1/output" ++ show utcNow ++ ".txt") . ppShowList . solve . parse $ contents
 
-parse = id
+data Vec3 = Vec3
+  { x :: Int,
+    y :: Int,
+    z :: Int }
+instance Show Vec3 where
+  show Vec3 {..} = show x ++ "," ++ show y ++ "," ++ show z
+
+data Block = Block
+  {
+    start :: Vec3,
+    end :: Vec3
+  }
+instance Show Block where
+  show Block {..} = show start ++ "~" ++ show end
+
+parse = lines >>> fmap (splitOneOf ",~" >>> fmap read >>> toBlock)
+  where toBlock [x1,y1,z1,x2,y2,z2] = Block { start = Vec3 { x=x1, y=y1, z=z1 }, end = Vec3 { x=x2, y=y2, z=z2 }}
+
 
 solve = id
