77169a4ef60ed0813a498ee12c81bf96e02e4297
Pavel Kalugin <p.kalugin@paralect.com>
2026-08-15 20:33:23 +0300

    [23.1] Done!

--- src/23_1/Main.hs
+++ src/23_1/Main.hs
@@ -12,13 +12,15 @@ import qualified Data.Vector as V
 main :: IO ()
 main = do
   utcNow   <- getCurrentTime
-  contents <- readFile "src/23_1/input_test.txt"
-  writeFile ("src/23_1/output" ++ show utcNow ++ ".txt") . show . solve . parse $ contents
+  contents <- readFile "src/23_1/input.txt"
+  writeFile ("src/23_1/output" ++ show utcNow ++ ".txt") . show . result . solve . parse $ contents
 
 parse :: String -> Matrix Char
 parse = lines >>> M.fromLists
 
--- solve :: Matrix Char -> Int
+result = length >>> subtract 1
+
+solve :: Matrix Char -> [(Int, Int)]
 solve m = bt [startPos]
   where
     startPos :: (Int, Int)
@@ -33,8 +35,15 @@ solve m = bt [startPos]
         cur :: (Int, Int)
         cur = unsafeHead path
 
+        posList = [first (+1), first (subtract 1), second (+1), second (subtract 1)]
+        charList = [flip elem ".<>v", flip elem ".<>^", flip elem ".>^v", flip elem ".<v^"]
+        requirements = zip posList charList
+
         candidates :: [(Int, Int)]
-        candidates = filter (not . flip elem path) . filter (get >>> \x -> isJust x && fromJust x `elem` ".^<>v") . map (\f -> f cur) $ [first (+1), first (subtract 1), second (+1), second (subtract 1)]
+        candidates = filter (not . flip elem path) .  map (\(f,_) -> f cur) . filter qualify $ requirements
+
+        qualify :: ((Int, Int) -> (Int, Int), Char -> Bool) -> Bool
+        qualify (f, ch) = f >>> get >>> fmap ch >>> fromMaybe False $ cur
 
         get :: (Int, Int) -> Maybe Char
         get (x, y) = M.safeGet x y m
